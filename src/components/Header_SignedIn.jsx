e990848501790dfdf1f79247b7c4dd7905003a2e
Atina <kellieatina@gmail.com>
2025-06-24 19:25:22 +0300

    Refactor Header component and add signed-in version with updated navigation links

--- src/components/Header_SignedIn.jsx
+++ src/components/Header_SignedIn.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+// For actual navigation in a React app, you'd use react-router-dom's Link component
+// import { Link } from 'react-router-dom';
 
-function Header(){
-    return(
-        <header className="header">
+function Header() {
+  return (
+    <header className="header">
       <div className="nav-container">
         <div className="logo">
           {/* Image paths are relative to the public folder */}
@@ -18,18 +20,20 @@ function Header(){
               For now, they retain original href for reference.
               Consider using `NavLink` from react-router-dom for active class highlighting.
             */}
-            <li><a href="/App.jsx">Home</a></li>
-            <li><a href="/pages/recipe.html">Recipes</a></li>
+            <li><a href="/pages/HomePage_SignedIn.html">Home</a></li>
+            <li><a href="/pages/recipe-S.html">Recipes</a></li>
+            <li><a href="/pages/meal-planner.html">Meal Plans</a></li>
+            <li><a href="/pages/favourites.html">Favourites</a></li>
             <li><a href="#" className="active">About</a></li> {/* 'About' typically handled by React Router */}
           </ul>
         </nav>
         <div className="auth-buttons">
-          <a href="/pages/index.html" className="btn-signin">Sign In</a>
-          <a href="/pages/Profile.html" className="btn-started">Get Started</a>
+          <a href="/pages/index.html" className="btn-signin">Log Out</a>
+          <a href="/pages/Profile.html" className="btn-started">Profile</a>
         </div>
       </div>
     </header>
-    );
+  );
 }
 
 export default Header;
