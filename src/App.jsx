7f307c8fdf1ac29b1017d2d3a0524ecd1e0946fe
Atina <kellieatina@gmail.com>
2025-06-29 19:11:30 +0300

    Refactor application structure: - Updated routing in App.jsx to include public and signed-in routes. - Removed unused components and styles (AppSections, About, Home, SignIn, SignUp). - Created new page components for HomePage and HomePageSignedIn. - Cleaned up navigation links and added a catch-all route for 404 errors.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import './index.css'; // Import your common styles globally
+
+// Import your page components (will create these below)
+import HomePage from './pages/HomePage';
+import HomePageSignedIn from './pages/HomePageSignedIn';
+import AboutUs from './pages/AboutUs';
+import AboutUsSignedIn from './pages/AboutUsSignedIn';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Quiz from './pages/Quiz';
+import Recipes from './pages/Recipes';
+import RecipesSignedIn from './pages/RecipesSignedIn';
+import MealPlanner from './pages/MealPlanner';
+import Favourites from './pages/Favourites';
+import Profile from './pages/Profile';
+import RecipeView from './pages/RecipeView';
+import RecipeViewSignedIn from './pages/RecipeViewSignedIn';
+import ShoppingList from './pages/ShoppingList';
 
 
-import Home from '../src/pages/Home'; 
-import SignUp from '../src/pages/SignUp'
 function App() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          {/* Add more links as you convert more pages */}
-        </ul>
-      </nav>
+    <>
+      {/* You can put a common Header component here if it's the same for all pages,
+          otherwise, include it within each page component */}
 
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        {/* Fallback for undefined routes */}
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipe-view" element={<RecipeView />} /> {/* Adjust this for dynamic IDs */}
+
+        {/* Signed-In Routes */}
+        <Route path="/home-signed-in" element={<HomePageSignedIn />} />
+        <Route path="/about-signed-in" element={<AboutUsSignedIn />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/recipes-signed-in" element={<RecipesSignedIn />} />
+        <Route path="/meal-planner" element={<MealPlanner />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/recipe-view-signed-in" element={<RecipeViewSignedIn />} /> {/* Adjust for dynamic IDs */}
+        <Route path="/shopping-list" element={<ShoppingList />} />
+
+
+        {/* Catch-all for 404 Not Found */}
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
-    </Router>
+
+      {/* You can put a common Footer component here */}
+    </>
   );
 }
 
