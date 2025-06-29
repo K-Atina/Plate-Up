1c9a603fd355b2128fdcd07e5da0220c2b852460
Atina <kellieatina@gmail.com>
2025-06-29 21:54:58 +0300

    Problem: Issue with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,12 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import './index.css'; // Import your common styles globally
-
-/*Public pages */
-import HomePage from './pages/HomePage';
-import AboutUs from './pages/AboutUs';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Recipes from './pages/Recipe';
-
-/*Signed-in pages */
-import HomePageSignedIn from './pages/HomePageSignedIn';
-import AboutUsSignedIn from './pages/AboutUs_S';
-import Quiz from './pages/Quiz';
-import RecipesSignedIn from './pages/Recipe_S';
-import MealPlanner from './pages/MealPlanner';
-import Favourites from './pages/Favourites';
-import Profile from './pages/Profile';
-import RecipeView from './pages/RecipeView';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/recipes" element={<Recipes />} />
-
-        {/* Signed-In Routes */}
-        <Route path="/home-signed-in" element={<HomePageSignedIn />} />
-        <Route path="/about-signed-in" element={<AboutUsSignedIn />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/recipes-signed-in" element={<RecipesSignedIn />} />
-        <Route path="/meal-planner" element={<MealPlanner />} />
-        <Route path="/favourites" element={<Favourites />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/recipe-view" element={<RecipeView />} />
-        <Route path="/shopping-list" element={<ShoppingList />} />
-
-
-        {/* Catch-all for 404 Not Found */}
-        <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<div style={{ padding: '2rem', fontSize: '1.5rem' }}>✅ Root Route Works</div>} />
+      <Route path="*" element={<div>🚫 Not Found</div>} />
+    </Routes>
   );
 }
 
