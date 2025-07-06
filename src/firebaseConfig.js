ef8d722df13fbf75835f9202ece5b84eda80beaf
Atina <kellieatina@gmail.com>
2025-07-07 01:44:47 +0300

    refactor: clean up firebaseConfig.js by removing unused comments and SDK references

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,11 +2,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyATMVwcPydDqMjKTKQfVgimwARJvWgg1jY",
   authDomain: "plateup-app-8ec8d.firebaseapp.com",
